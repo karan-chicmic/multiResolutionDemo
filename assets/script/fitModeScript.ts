d9bad9fec93b7a732dbf2abade84590b02d7f024
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-05-01 14:18:02 +0530

    working, but not working with live parent size change

--- assets/script/fitModeScript.ts
+++ assets/script/fitModeScript.ts
@@ -15,52 +15,30 @@ const cordinates = {
 
 @ccclass("fitModeScript")
 export class fitModeScript extends Component {
-    private fitMode;
+    fitMode: fitModes = fitModes.None;
     state: boolean = true;
     ratio = 0;
 
     @property({ type: Enum(fitModes) })
-    modes: fitModes = Enum(fitModes).None;
-    // @property({ type: Boolean })
-    // set statex(val) {
-    //     this.state = val;
-    //     this.node.active = this.state;
-    // }
-    // get statex() {
-    //     return this.state;
-    // }
-    @property({ type: Enum(fitModes) })
     set mode(val) {
         this.fitMode = val;
+        console.log(this.fitMode);
+        this.onaction(val);
     }
     get mode() {
+        console.log("get fit mode", this.fitMode);
         return this.fitMode;
     }
 
-    start() {
+    start() {}
+    onaction(val: fitModes) {
         const parentWidth = this.getSize(this.node.parent).width;
         const parentHeight = this.getSize(this.node.parent).height;
         const childWidth = this.getSize(this.node).width;
         const childHeight = this.getSize(this.node).height;
-        console.log("parent", parentWidth, parentHeight);
-        console.log("child", childWidth, childHeight);
 
-        switch (this.modes) {
-            case fitModes.FitHeight: {
-                console.log("fit height");
-                this.ratio = this.getFitHeightRatio(parentHeight, childHeight);
-                this.node.setScale(1, this.ratio);
-
-                break;
-            }
-            case fitModes.FitWidth: {
-                console.log("fit width");
-                this.ratio = this.getFitWidthRatio(parentWidth, childWidth);
-                this.node.setScale(this.ratio, 1);
-                break;
-            }
-            case fitModes.FitInside: {
-                console.log("fit inside");
+        switch (this.fitMode) {
+            case 1: {
                 if (parentWidth < parentHeight) {
                     this.ratio = this.getFitWidthRatio(parentWidth, childWidth);
                     this.node.setScale(this.ratio, 1);
@@ -71,9 +49,18 @@ export class fitModeScript extends Component {
                     break;
                 }
             }
+            case 2: {
+                this.ratio = this.getFitWidthRatio(parentWidth, childWidth);
+                this.node.setScale(this.ratio, 1);
+                break;
+            }
+            case 3: {
+                this.ratio = this.getFitHeightRatio(parentHeight, childHeight);
+                this.node.setScale(1, this.ratio);
+                break;
+            }
         }
     }
-
     update(deltaTime: number) {}
 
     getSize(node: Node) {
